360f7959d531c4621c24b28ca9dbe6cae53551e1
Jesus Miramontes <4751856+JesusMiramontes@users.noreply.github.com>
2023-03-22 20:42:06 -0600

    List Hotels.

--- src/main/java/com/miramontes/hoteldemorest/client/HotelClient.java
+++ src/main/java/com/miramontes/hoteldemorest/client/HotelClient.java
@@ -1,7 +1,9 @@
 package com.miramontes.hoteldemorest.client;
 
 import com.miramontes.hoteldemorest.client.generated.GetByIdRequest;
+import com.miramontes.hoteldemorest.client.generated.GetHotelListRequest;
 import com.miramontes.hoteldemorest.client.generated.ResponseHotel;
+import com.miramontes.hoteldemorest.client.generated.ResponseHotelList;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 
 public class HotelClient extends WebServiceGatewaySupport {
@@ -11,5 +13,10 @@ public class HotelClient extends WebServiceGatewaySupport {
         request.setId(id);
 
         return (ResponseHotel) getWebServiceTemplate().marshalSendAndReceive(request);
+    }
+
+    public ResponseHotelList getAll() {
+        return (ResponseHotelList)
+                getWebServiceTemplate().marshalSendAndReceive(new GetHotelListRequest());
     }
 }
--- src/main/java/com/miramontes/hoteldemorest/controller/HotelController.java
+++ src/main/java/com/miramontes/hoteldemorest/controller/HotelController.java
@@ -2,8 +2,10 @@ package com.miramontes.hoteldemorest.controller;
 
 import com.miramontes.hoteldemorest.client.HotelClient;
 import com.miramontes.hoteldemorest.client.generated.ResponseHotel;
+import com.miramontes.hoteldemorest.client.generated.ResponseHotelList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class HotelController {
     @Autowired HotelClient hotelClient;
 
-    @GetMapping
-    public ResponseHotel hello() {
-        return hotelClient.getById(1);
+    @GetMapping("/")
+    public ResponseHotelList list() {
+        return hotelClient.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseHotel byId(@PathVariable Integer id) {
+        return hotelClient.getById(id);
     }
 }
